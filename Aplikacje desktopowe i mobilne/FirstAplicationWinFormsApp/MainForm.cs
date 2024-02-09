6d2aef1983c3d2491930a099f5b5dd398e65a2dc
g <g@g.pl>
2024-02-09 08:46:58 +0100

    koniec zajęć

--- Aplikacje desktopowe i mobilne/FirstAplicationWinFormsApp/MainForm.cs
+++ Aplikacje desktopowe i mobilne/FirstAplicationWinFormsApp/MainForm.cs
@@ -11,7 +11,37 @@ namespace FirstAplicationWinFormsApp
 
         private void buttonFirst_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Witaj w tym programie",
+            string message = "Witaj w tym programie\n";
+
+            message += "Ulubione przedmioty:\n";
+            if (checkBoxMathematics.Checked ) 
+            {
+                message += "* Matematyka\n";
+            }
+            if (checkBoxGerman.Checked)
+            {
+                message += "* Jêzyk niemiecki\n";
+            }
+
+            if (radioButtonPizzaYes.Checked ) 
+            {
+                message += "Lubiê pizze z ananasem\n";
+            }
+            else
+            {
+                message += "Nie lubiê pizzy z ananasem\n";
+            }
+
+            message += "Twoje imiê to: " + textBoxName.Text + "\n";
+            textBoxName.Text = "";
+
+            if (comboBoxFavoriteGame.SelectedIndex != -1 )
+            {
+                message+="Ulubiona gra to "
+                    + comboBoxFavoriteGame.Text + "\n";
+            }
+
+            MessageBox.Show(message,
                 "Hello",
                 MessageBoxButtons.OKCancel,
                 MessageBoxIcon.Information, 
